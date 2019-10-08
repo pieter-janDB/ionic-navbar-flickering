5869fd19ebdde6f5ef504edb4775e392030e5106
Pieter-Jan De Bruyne <pieterjan@the-craft.be>
2019-10-08 12:25:10 +0200

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+   public url = '/list';
+  
+  constructor(
+    public router: Router,
+    public navCtrl: NavController
+  ) {}
+  ngOnInit(){}
 
+  navigate() {
+    this.router.navigate([this.url]);
+  }
+
+  navigateForwards() {
+    this.navCtrl.navigateForward(this.url);
+  }
+
+  navigateBackwards() {
+    this.navCtrl.navigateBack(this.url);
+  }
+
+  navigateroot() {
+    this.navCtrl.navigateRoot(this.url);
+  }
+
+  navigateByUrl(){
+    this.router.navigateByUrl(this.url);
+  }  
 }
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -6,34 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  public url = '/home';
+  
+  constructor(
+    public router: Router,
+    public navCtrl: NavController
+  ) {}
+
+  ngOnInit(){}
+
+  navigate() {
+    this.router.navigate([this.url]);
   }
 
-  ngOnInit() {
+  navigateForwards() {
+    this.navCtrl.navigateForward(this.url);
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+
+  navigateBackwards() {
+    this.navCtrl.navigateBack(this.url);
+  }
+
+  navigateroot() {
+    this.navCtrl.navigateRoot(this.url);
+  }
+
+  navigateByUrl(){
+    this.router.navigateByUrl(this.url);
+  }  
 }
